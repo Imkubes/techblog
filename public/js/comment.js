eac20d47e635545565c423a7036e370abe54c2a3
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-05-09 23:32:31 -0500

    can add comments but stuck on how to view the comment under the post

---
+++ public/js/comment.js
@@ -0,0 +1,28 @@
+const addCommentSubmit = async function (event) {
+  event.preventDefault();
+
+  const comment = document.querySelector('#comment');
+  const post = document.querySelector('#post-id');
+
+  const input = [];
+
+    const response = await fetch("/api/comment/", {
+      method: "POST",
+      body: JSON.stringify({
+        contents: comment.value,
+        post_id: post.value,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    
+  if (response.ok) {
+    document.location.replace("/homepage");
+    alert('Added to the list!')
+  } else {
+    alert("Failed to post");
+  }
+};
+
+document
+  .querySelector("#comment-form")
+  .addEventListener("submit", addCommentSubmit);
